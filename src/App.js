9fdd22bcd668e3f50832c75d5e314ee023e13da8
Mikelle Wade <mikelle.wade@gadadevelopersacademy.org>
2023-06-14 15:04:52 -0400

    implemented prop passing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 // import logo from './logo.svg';
-import './App.css';
-import Entry from './components/Entry';
+import "./App.css";
+import Entry from "./components/Entry";
+import data from "./data";
+
 function App() {
+
+  const entryComponents = data.map( entry => {
+    return <Entry key={entry.id} entry={entry} /> 
+  })
+
   return (
     <div className="App">
       <nav className="navbar">
-          <div className="logoContainer">
-              <span className="navbarTitle">my travel journal.</span>
-          </div>
+        <div className="logoContainer">
+          <span className="navbarTitle">my travel journal.</span>
+        </div>
       </nav>
-      <div className='contentContainer'>
-        <Entry /> 
-        <Entry />
-        <Entry />
+      <div className="contentContainer">
+        {entryComponents}
       </div>
     </div>
   );
--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,41 +1,42 @@
-import React from 'react';
+import React from "react";
 
-function Entry(props) {
-  console.log(props)
+function Entry({ entry }) {
+  console.log(entry)
+//   const { title, location, startDate, img, googleMapsUrl, description } = props;
+//   console.log("Image Link", img);
   return (
-      <div>
-          <div className="entryContainer">
-              <div>
-                  <div id={"carousel"} className="carousel slide" data-ride="carousel">
-                  <div className="carousel-inner">
-                      <div className="carousel-item active">
-                      <img className="d-block w-100 travelImg" src={''} alt="Destination Img"/>
-                      </div>
-                      <div className="carousel-item">
-                      {/* <img className="d-block w-100 travelImg" src={path + props.entry.imgs.img2}alt="Second slide"/> */}
-                      </div>
-                  </div>
-                  </div>               
+    <div>
+      <div className="entryContainer">
+        <div>
+          <div id={"carousel"} className="carousel slide" data-ride="carousel">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img
+                  className="d-block w-100 travelImg"
+                  src={require(`../imgs/${entry.img}`)}
+                  alt={`Attraction in ${entry.location}`}
+                />
               </div>
-              
-              <div className="infoContainer">
-                  {/* <img src= "./logos/Pinpoint.png"/> */}
-                  <span className="destination">Destination</span>
-                  <a className="googleLink" href='#'>View on Google Maps</a>
-                  
-                  <p className="attraction">Attraction</p>
-                  <p className="year">Trip Year</p>
-                  <p className="description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  </p>
-              </div>
-          
+              <div className="carousel-item"></div>
+            </div>
           </div>
-          <div className="breaker"></div>
+        </div>
+
+        <div className="infoContainer">
+          {/* <img src= "./logos/Pinpoint.png"/> */}
+          <span className="destination">{entry.location}</span>
+          <a className="googleLink" href={entry.googleMapsUrl}>
+            View on Google Maps
+          </a>
+
+          <p className="attraction">{entry.title}</p>
+          <p className="year">{entry.startDate}</p>
+          <p className="description">{entry.description}</p>
+        </div>
       </div>
-  )
+      <div className="breaker"></div>
+    </div>
+  );
 }
 
-
-
-export default Entry
+export default Entry;
